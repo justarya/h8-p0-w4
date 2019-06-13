cb1986398c7d9c2955b6a74034e2a7f9479831b9
A R Y A <just1arya@gmail.com>
2019-06-13 15:20:57 +0700

    Fix exercise 10

--- exercise-10.js
+++ exercise-10.js
@@ -1,15 +1,19 @@
 function changeMe(arr) {
   // you can only write your code here!
-  for (let i = 0; i < arr.length; i++) {
-    if(arr[i][3] === undefined) arr[i][3] = 'Invalid Birth Year';
-    let obj = {
-      firstName: arr[i][0],
-      lastName: arr[i][1],
-      gender: arr[i][2],
-      age: arr[i][3],
+  if(arr.length > 0){
+    for (let i = 0; i < arr.length; i++) {
+      if(arr[i][3] === undefined) arr[i][3] = 'Invalid Birth Year';
+      let obj = {
+        firstName: arr[i][0],
+        lastName: arr[i][1],
+        gender: arr[i][2],
+        age: arr[i][3],
+      }
+      console.log(i+1+'.',arr[i][0],arr[i][1]);
+      console.log(obj);
     }
-    console.log(i+1+'.',arr[i][0],arr[i][1]);
-    console.log(obj);
+  }else{
+    console.log("");
   }
 }
 
